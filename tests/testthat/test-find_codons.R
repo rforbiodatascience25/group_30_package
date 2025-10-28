db9cc4643f66842e664ec8b28de8f99067804409
isab4635 <isabella.t.chr@gmail.com>
2025-10-28 10:10:02 +0100

    fix: test of find_codons

--- tests/testthat/test-find_codons.R
+++ tests/testthat/test-find_codons.R
@@ -1,4 +1,6 @@
 test_that("extracting codons work", {
-  expect_equal("ATCGTTACCGTCCGT", c("ATC", "GTT", "ACC", "GTC", "CGT"))
-  expect_equal("ATCGTTACCGTCCG", c("ATC", "GTT", "ACC", "GTC"))
+  expect_equal(find_codons("ATCGTTACCGTCCGT"),
+               c("ATC", "GTT", "ACC", "GTC", "CGT"))
+  expect_equal(find_codons("ATCGTTACCGTCCG"),
+               c("ATC", "GTT", "ACC", "GTC"))
 })
