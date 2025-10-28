0d14a67e6daeb1119dc31bb93cab038a47c4bc2c
malaikakhiljee <s245618@dtu.dk>
2025-10-28 09:06:04 +0100

    Addition and description of the object standard_codon

---
+++ R/data.R
@@ -0,0 +1,7 @@
+#' Standard Codon
+#'
+#' Vector containing the standard codon and the corresponding aminoacid.
+#'
+#'
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"standard_codon"
