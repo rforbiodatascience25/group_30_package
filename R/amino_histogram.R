aad9cc5abf82a56b76b356f630f5c6df763f4022
karo3420 <s244854@dtu.dk>
2025-10-28 11:28:52 +0100

    the making of function 5

---
+++ R/amino_histogram.R
@@ -0,0 +1,31 @@
+#' Generate histogram of amino acids
+#'
+#' @param amino_sequence An amino acid sequence string
+#'
+#' @returns histogram_ggplot
+#' @export
+
+amino_histogram <- function(amino_sequence){
+  amino_unique <- amino_sequence |>
+    stringr::str_split(pattern = stringr::boundary("character"), simplify = TRUE) |>
+    as.character() |>
+    unique()
+
+  counts <- sapply(amino_unique,
+                   function(amino_acid) stringr::str_count(string = amino_sequence,
+                                                           pattern =  amino_acid)) |>
+    as.data.frame()
+
+  colnames(counts) <- c("Counts")
+  counts[["Amino_sequence"]] <- rownames(counts)
+
+  histogram_plot <- counts |>
+    ggplot2::ggplot(ggplot2::aes(x = Amino_sequence,
+                                 y = Counts,
+                                 fill = Amino_sequence)) +
+    ggplot2::geom_col() +
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position = "none")
+
+  return(histogram_plot)
+}
---
+++ tests/testthat/test-amino_histogram.R
@@ -0,0 +1,3 @@
+test_that("amino_histogram makes a histogram", {
+  expect_equal(2 * 2, 4)
+})
